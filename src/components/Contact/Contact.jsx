e1dddb768a824c6b99caadd42aa0a30590fef851
Aphichat_Rungruang <161464886+AphichatRungruang@users.noreply.github.com>
2024-05-17 01:56:38 +0700

    updatepj

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,23 +8,22 @@ export const Contact = () => {
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
         <h2>Contact</h2>
-        <p>Feel free to reach out!</p>
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
           <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:myemail@email.com">myemail@email.com</a>
+          <a href="mailto:Aphichatrungruang@gmail.com">Aphichatrungruang@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/myname">linkedin.com/myname</a>
+          <a href="https://www.linkedin.com/in/aphichat-rungruang-311a99193/">linkedin.com/in/aphichat-rungruang</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.github.com/myname">github.com/myname</a>
+          <a href="https://www.github.com">github.com</a>
         </li>
       </ul>
     </footer>
